850b76369ecbcc5b7f06583292dffcdfaa76e8da
jhonned01 <jhonned01@gmail.com>
2021-05-25 02:28:09 -0500

    vista mas o menos falta funcinoalidad

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
-
-export default function Home() {
+import ProducFeed from "../components/ProducFeed";
+export default function Home({ products }) {
+  console.log("====================================");
+  console.log(products);
+  console.log("====================================");
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
@@ -15,7 +18,19 @@ export default function Home() {
         <Banner />
 
         {/* Produc */}
+
+        <ProducFeed products={products} />
       </main>
     </div>
   );
 }
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  return { props: { products } };
+}
+
+// GET >> https://fakestoreapi.com/products
